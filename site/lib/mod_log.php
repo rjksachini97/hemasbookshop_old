9353bd53ee5b094ffcb13ca93db78ba36335b530
rjksachini97 <82650362+rjksachini97@users.noreply.github.com>
2021-04-18 07:07:01 +0530

    change register customer login

--- site/lib/mod_log.php
+++ site/lib/mod_log.php
@@ -1,6 +1,6 @@
 <?php
 	require_once("dbconnection.php");
-	$dbobj = DB::coonect();
+	$dbobj = DB::connect();
 
 if(isset($_GET['ischecklogin'])){
 	$cus_email = $_POST['email'];
@@ -12,18 +12,18 @@ if(isset($_GET['ischecklogin'])){
 
 }
 
-//elseif (isset($_POST['email'])){
-	//$cus_email = $_POST['email'];
-	//$cus_pass = md5($_POST['pass']);
-	//$sql = "SELECT * FROM tbl_reg_customer WHERE cus_email='$cus_email' AND cus_pass='$cus_pass';";
-	//$resultlogin = $dbobj->query($sql);
+elseif (isset($_POST['email'])){
+	$cus_email = $_POST['email'];
+	$cus_pass = md5($_POST['pass']);
+	$sql = "SELECT * FROM tbl_reg_customer WHERE cus_email='$cus_email' AND cus_pass='$cus_pass';";
+	$resultlogin = $dbobj->query($sql);
 
-	//if($resultlogin->num_rows == 1){
-		//session_start();
-		//$_SESSION['session_cus'] = $resultlogin->fetch_assoc();
-		//header("Location: ../index.php#services");
-	//}
+	if($resultlogin->num_rows == 1){
+		session_start();
+		$_SESSION['session_cus'] = $resultlogin->fetch_assoc();
+		header("Location: ../index.php#services");
+	}
 	
-//}
+}
 
 ?>
