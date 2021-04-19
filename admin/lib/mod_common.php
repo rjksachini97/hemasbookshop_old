f94b1b1b8e8845afe89e610f69b38144a0b7d0a2
rjksachini97 <82650362+rjksachini97@users.noreply.github.com>
2021-04-19 21:51:00 +0530

    update common functions

--- admin/lib/mod_common.php
+++ admin/lib/mod_common.php
@@ -45,9 +45,70 @@ function getUserName(){
 	$dbobj = DB::connect();
 	$table = "tbl_users";
 
-	$sql = "SELECT usr_name from $table WHERE usr_status=1;";
+	$sql = "SELECT usr_name from tbl_users WHERE usr_status=1;";
 
 	$result = $dbobj->query($sql);
 }
+
+/*------------------------Display Active User count----------------------*/
+function getActiveUserCount(){
+    $dbobj = DB::connect();
+    $sql = "SELECT count(usr_status) FROM tbl_users WHERE usr_status=1";
+    $result = $dbobj->query($sql);
+    if($dbobj->errno){
+        echo("SQL Error : " .$dbobj->error);
+        exit;
+    }
+    $rec = $result->fetch_array();
+    echo ($rec[0]);
+
+    $dbobj->close();
+}
+
+/*------------------------Display Register customer count----------------------*/
+function getRegCustomerCount(){
+    $dbobj = DB::connect();
+    $sql = "SELECT count(cus_id) FROM tbl_reg_customer";
+    $result = $dbobj->query($sql);
+    if($dbobj->errno){
+        echo("SQL Error : " .$dbobj->error);
+        exit;
+    }
+    $rec = $result->fetch_array();
+    echo ($rec[0]);
+
+    $dbobj->close();
+}
+
+/*------------------------Display Register customer count----------------------*/
+function getNPOrderCount(){
+    $dbobj = DB::connect();
+    $sql = "SELECT count(order_id) FROM tbl_newspaper_order";
+    $result = $dbobj->query($sql);
+    if($dbobj->errno){
+        echo("SQL Error : " .$dbobj->error);
+        exit;
+    }
+    $rec = $result->fetch_array();
+    echo ($rec[0]);
+
+    $dbobj->close();
+}
+
+/*------------------------Display Register customer count----------------------*/
+function getOutofStockCount(){
+    $dbobj = DB::connect();
+    $sql = "SELECT count(*)newsp_id FROM tbl_newspaper WHERE newsp_qty='0'";
+    $result = $dbobj->query($sql);
+    if($dbobj->errno){
+        echo("SQL Error : " .$dbobj->error);
+        exit;
+    }
+    $rec = $result->fetch_array();
+    echo ($rec[0]);
+
+    $dbobj->close();
+}
+
 
 ?>
