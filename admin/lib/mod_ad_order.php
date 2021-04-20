1eae2344b8e75225ddf70f0a1a291493728ddb82
rjksachini97 <82650362+rjksachini97@users.noreply.github.com>
2021-04-20 08:11:19 +0530

    updated ad order view query

--- admin/lib/mod_ad_order.php
+++ admin/lib/mod_ad_order.php
@@ -7,28 +7,26 @@ if(isset($_GET["type"])){
 }
  function viewAllAdOrders() {
  	$table = <<<EOT
- 				(SELECT abook.ad_book_id,cus.cus_name,amode.newsad_mode,clr.adcolour_name,np.newsp_name,ac.newsac_category,act.adcattype_desc,
- 		md.admode_details_size,abook.current_date,abook.adpub_date,abook.ad_description,abook.ad_wc,abook.ad_tot_price,abook.ad_pay_status,
- 		abook.ad_book_status FROM tbl_ad_booking abook
-			JOIN tbl_reg_customer cus ON abook.cus_id = cus.cus_id
-			JOIN tbl_news_ad_mode amode ON abook.newsad_mode_id = amode.newsad_mode_id
-			JOIN tbl_ad_colour clr ON abook.adcolour_id = clr.adcolour_id
-			JOIN tbl_newspaper np ON abook.newsp_id = np.newsp_id
-			JOIN tbl_news_ad_category ac ON abook.newsac_id = ac.newsac_id
-			JOIN tbl_news_adcat_type act ON abook.adcattype_id = act.adcattype_id
-			JOIN tbl_ad_modes_details md ON abook.admode_details_id = md.admode_details_id WHERE
-			abook.ad_book_status=1 ORDER BY ad_book_id
+ 				(SELECT ado.*,cus.cus_name,abook.adcolour_name,abook.newsp_name,abook.newsac_category,abook.adcattype_desc,abook.admode_details_size,abook.ad_description,abook.ad_wc,abook.ad_pay_status,abook.ad_book_status,email.email_id,email.email_status,sms.sms_id,sms.sms_status FROM tbl_ad_order ado 
+					JOIN tbl_reg_customer cus ON ado.cus_id = cus.cus_id
+					JOIN tbl_ad_booking abook ON ado.ad_book_id = abook.ad_book_id
+                    JOIN tbl_email email ON ado.adorder_id = email.adorder_id
+                    JOIN tbl_sms sms ON ado.adorder_id = sms.adorder_id
+					WHERE abook.ad_book_status=1 ORDER BY ad_book_id
  			)temp
 EOT;
- 			$primaryKey = 'ad_book_id';
+ 			$primaryKey = 'adorder_id';
 
  				$columns = array(
-	    			array( 'db' => 'ad_book_id','dt' => 0 ),
+	    			array( 'db' => 'adorder_id','dt' => 0 ),
 	    			array( 'db' => 'cus_name','dt' => 1 ),
-	    			array( 'db' => 'current_date','dt' => 2 ),
-	    			array( 'db' => 'adpub_date','dt' => 3 ),
-	    			array( 'db' => 'ad_tot_price','dt' => 4 ),
-	    			array( 'db' => 'ad_pay_status','dt' => 5 )
+	    			array( 'db' => 'adorder_date','dt' => 2 ),
+	    			array( 'db' => 'publish_date','dt' => 3 ),
+	    			array( 'db' => 'adorder_price','dt' => 4 ),
+	    			array( 'db' => 'ad_order_status','dt' => 5 ),
+	    			array( 'db' => 'email_status','dt' => 6 ),
+	    			array( 'db' => 'sms_status','dt' => 7 )
+	    			
 				);
 
 				// SQL server connection information
