fe76c3b4ff618ed742ff4d34d7421810ae408f55
rjksachini97 <82650362+rjksachini97@users.noreply.github.com>
2021-04-20 23:16:19 +0530

    changed the code

--- site/lib/logout.php
+++ site/lib/logout.php
@@ -1,17 +1,31 @@
+
 <?php
+// Initialize the session
 session_start();
-if(isset($_GET["type"])){
-	$type = $_GET["type"];
-	$type();
-}
-
-function logout(){
-	if (!isset($_SESSION["user"])){
-		header("location:../index.php");
-}
-
-unset($_SESSION["user"]);
+ 
+// Destroy the session.
 session_destroy();
-header("location:../index.php");
-}
+ 
+// Redirect to login page
+header("location: ../index.php");
+exit;
 ?>
+<!--
+<?php
+//session_start();
+//if(isset($_GET["type"])){
+	//$type = $_GET["type"];
+	//$type();
+//}
+
+//function logout(){ 
+	//if (!isset($_SESSION["user"])){
+		//header("location:../index.php");
+//}
+
+//unset($_SESSION["user"]);
+//session_destroy();
+//header("location:../index.php");
+//}
+?>  
+-->
