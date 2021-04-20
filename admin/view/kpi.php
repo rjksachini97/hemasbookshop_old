8320fd757eb8ee57e77ad540fa6a905236cc0100
rjksachini97 <82650362+rjksachini97@users.noreply.github.com>
2021-04-20 08:13:58 +0530

    remove cards

--- admin/view/kpi.php
+++ admin/view/kpi.php
@@ -1,11 +1,15 @@
+
+
+
 <!-- Breadcrumbs-->
-<div class="row d-sm-flex align-items-center justify-content-between mb-4">
+<!--<div class="row d-sm-flex align-items-center justify-content-between mb-4">
   <div class="col-xl-8 col-lg-7">  <ol class="breadcrumb">
       <li class="breadcrumb-item">
         <a href="#">Dashboard</a>
       </li>
       <li class="breadcrumb-item active">Overview</li>
-    </ol></div>   
+    </ol></div>
+-->   
   <!-- Page Heading -->
 <!--<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-secondary shadow-sm" id="btnallreports">
   <i class="fas fa-download fa-sm text-white-50"></i> Generate Report  </a>
@@ -16,19 +20,20 @@ $(document).ready(function(){
   });
 });
 </script>  -->
-</div>  
+<!--</div>   -->
 
 <!-- Content Row -->
-<div class="row">
+<!--<div class="row">  -->
 
               <!-- Active Users Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+<!--            <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-secondary text-uppercase mb-2">Active Users</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                      <?php    //ActiveUserCount() ?>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-users fa-2x text-gray-600"></i>
@@ -36,10 +41,10 @@ $(document).ready(function(){
                   </div>
                 </div>
               </div>
-            </div>
-
+            </div> 
+-->
               <!-- Orders Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+ <!--           <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -54,9 +59,9 @@ $(document).ready(function(){
                 </div>
               </div>
             </div>
-
+-->
               <!-- Out of Stocks Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+<!--            <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-danger shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -72,215 +77,6 @@ $(document).ready(function(){
               </div>
             </div>
 
-  <!-- Employee management-->
-<!--  <div class="col-xl-2 col-md-6 mb-4">
-    <div class="card shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-auto">
-            <i class="fas fa-user-tie fa-4x text-gray-650"></i>
-          </div>
-        <div class="row">
-          <div class="col mr-2">
-            <div class="nav-item dropdown" >
-              <div class="h6 mb-0 font-weight-bold text-gray-800">
-                <a class="text-gray-700"> Employee Management</a>
-                <a class="nav-link dropdown-toggle-hidden text-secondary" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"aria-haspopup="true" aria-expanded="false">
-                </a>
-                <div class="dropdown-menu" aria-labelledby="pagesDropdown" data-parent="#accordionSidebar">
-                  <div class="bg-white py-2 dropdown-inner rounded">            
-                    <a class="dropdown-item menu" href="#" title="" id="lnknewemp">New Employees</a>
-                    <a class="dropdown-item menu" href="#" title="" id="lnkviewemp">View Employees</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div> -->
-
-  <!-- User Management -->
-<!--  <div class="col-xl-2 col-md-6 mb-4">
-    <div class="card shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-auto">
-            <i class="fas fa-users fa-4x text-gray-650"></i>
-          </div>
-        <div class="row">
-          <div class="col mr-2">
-            <div class="nav-item dropdown">
-              <div class="h6 mb-0 font-weight-bold text-gray-800">
-                <a class="text-gray-700" > User Management</a>
-                <a class="nav-link dropdown-toggle-hidden text-secondary" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                </a>
-                <div class="dropdown-menu" aria-labelledby="pagesDropdown" data-parent="#accordionSidebar">
-                  <div class="bg-white py-2 dropdown-inner rounded">             
-                    <a class="dropdown-item menu" href="#" title="" id="lnknewacc">New Account</a>
-                    <a class="dropdown-item menu" href="#" title="" id="lnkviewuser">View User</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>    -->     
-          
-<!-- Customer Management -->
-<!--  <div class="col-xl-2 col-md-6 mb-4">
-    <div class="card shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-auto">
-            <i class="fas fa-users fa-4x text-gray-650"></i>
-          </div>
-        <div class="row">
-          <div class="col mr-2">
-            <div class="nav-item dropdown">
-              <div class="h6 mb-0 font-weight-bold text-gray-800">
-                <a class="text-gray-700" > Customer Management</a>
-                <a class="nav-link dropdown-toggle-hidden text-secondary" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                </a>
-                <div class="dropdown-menu" aria-labelledby="pagesDropdown" data-parent="#accordionSidebar">
-                  <div class="bg-white py-2 dropdown-inner rounded">             
-                    <a class="dropdown-item menu" href="#" title="" id="lnknewcus">New Account</a>
-                    <a class="dropdown-item menu" href="#" title="" id="lnkviewcus">View Customer</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>  -->
-
-  <!-- Publication Company -->
-<!--  <div class="col-xl-2 col-md-6 mb-4">
-    <div class="card shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-auto">
-            <i class="fas fa-building fa-4x text-gray-650"></i>
-          </div>
-        <div class="row">
-          <div class="col mr-2">
-            <div class="nav-item dropdown">
-              <div class="h6 mb-0 font-weight-bold text-gray-800">
-                <a class="text-gray-700" > Publication Company</a>
-                <a class="nav-link dropdown-toggle-hidden text-secondary" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                </a>
-                <div class="dropdown-menu" aria-labelledby="pagesDropdown" data-parent="#accordionSidebar">
-                  <div class="bg-white py-2 dropdown-inner rounded">          
-                    <a class="dropdown-item menu" href="#" title="" id="lnknewpc">New Publication Company</a>
-                    <a class="dropdown-item menu" href="#" title="" id="lnkviewpc">View Publication Company</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>   -->
-
-  <!-- Seller -->
-<!--  <div class="col-xl-2 col-md-6 mb-4">
-    <div class="card shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-auto">
-            <i class="fas fa-user fa-4x text-gray-650"></i>
-          </div>
-        <div class="row">
-          <div class="col mr-2">
-            <div class="nav-item dropdown">
-              <div class="h6 mb-0 font-weight-bold text-gray-800">
-                <a class="text-gray-700" >Seller</a>
-                <a class="nav-link dropdown-toggle-hidden text-secondary" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                </a>
-                <div class="dropdown-menu" aria-labelledby="pagesDropdown" data-parent="#accordionSidebar">
-                  <div class="bg-white py-2 dropdown-inner rounded">          
-                    <a class="dropdown-item menu" href="#" title="" id="lnknewseller">New Seller</a>
-                    <a class="dropdown-item menu" href="#" title="" id="lnkviewseller">View Seller</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>   -->
-
-  <!-- Delivery Management -->
-<!--  <div class="col-xl-2 col-md-6 mb-4">
-    <div class="card shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-auto">
-            <i class="fas fa-truck fa-4x text-gray-650"></i>
-          </div>
-        <div class="row">
-          <div class="col mr-2">
-            <div class="nav-item dropdown">
-              <div class="h6 mb-0 font-weight-bold text-gray-800">
-                <a class="text-gray-700" > Delivery Management</a>
-                <a class="nav-link dropdown-toggle-hidden text-secondary" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                </a>
-                <div class="dropdown-menu" aria-labelledby="pagesDropdown" data-parent="#accordionSidebar">
-                  <div class="bg-white py-2 dropdown-inner rounded">          
-                    <a class="dropdown-item menu" href="#" title="" id="lnknewdelm">New Deliveryman</a>
-                    <a class="dropdown-item menu" href="#" title="" id="lnkviewdelm">View Deliveryman</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>   -->
-
-<!-- Order Management -->
-<!--  <div class="col-xl-2 col-md-6 mb-4">
-    <div class="card shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-auto">
-            <i class="fas fa-users fa-4x text-gray-650"></i>
-          </div>
-        <div class="row">
-          <div class="col mr-2">
-            <div class="nav-item dropdown">
-              <div class="h6 mb-0 font-weight-bold text-gray-800">
-                <a class="text-gray-700" > Order Management</a>
-                <a class="nav-link dropdown-toggle-hidden text-secondary" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                </a>
-                <div class="dropdown-menu" aria-labelledby="pagesDropdown" data-parent="#accordionSidebar">
-                  <div class="bg-white py-2 dropdown-inner rounded">             
-                    <a class="dropdown-item menu" href="#" title="" id="lnknewacc">New Account</a>
-                    <a class="dropdown-item menu" href="#" title="" id="lnkviewuser">View Order</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>   -->
       
 </div>
-
+-->
