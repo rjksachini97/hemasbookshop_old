299de550040683f4a840ddaf574fde470150f331
rjksachini97 <82650362+rjksachini97@users.noreply.github.com>
2021-04-20 18:42:11 +0530

    updated cus page

--- site/lib/mod_cus.php
+++ site/lib/mod_cus.php
@@ -38,4 +38,40 @@ function addNewCus(){
 	$dbobj->close();
 }
 
+function uploadSlipAd(){
+	$dbobj = DB::connect();
+
+	$ad_book_id = $_GET['ad_book_id'];
+	$img_name = $ad_book_id . ".jpg";
+	$img_temp = $_FILES['imgup']['tmp_name'];
+	$path = "../../images/BankSlips/AdBooking_slips/" . $img_name;
+	$sql = "UPDATE  tbl_ad_booking SET ad_img_slip='$img_name' WHERE ad_book_id=$ad_book_id";
+
+	if($dbobj->query($sql)){
+		move_uploaded_file($img_temp, $path);
+	}
+	// echo $dbobj->error;
+	// return 0;
+
+	header("Location: ../cus_profile.php");
+}
+
+function uploadSlipNp(){
+	$dbobj = DB::connect();
+
+	$np_book_id = $_GET['np_book_id'];
+	$img_name = $np_book_id . ".jpg";
+	$img_temp = $_FILES['imgup']['tmp_name'];
+	$path = "../../images/BankSlips/NpBooking_slips/" . $img_name;
+	$sql = "UPDATE  tbl_newspaper_booking SET np_slip_img='$img_name' WHERE np_book_id=$np_book_id";
+
+	if($dbobj->query($sql)){
+		move_uploaded_file($img_temp, $path);
+	}
+	// echo $dbobj->error;
+	// return 0;
+
+	header("Location: ../cus_profile.php");
+}
+
 ?>
